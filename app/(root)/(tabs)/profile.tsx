bf9438fe28c466bd1c8b422321f146dd944995e0
Abhishek Kumar <abhi1322jii@gmail.com>
2024-12-30 12:23:56 +0530

    feat: profile page logout function

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,63 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  ImageSourcePropType,
+  Alert,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { settings } from "@/constants/data";
+import { useGlobalContext } from "@/lib/global-provider";
+import { logout } from "@/lib/appwrite";
+
+interface SettingItemsProps {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress?: () => void;
+  textStyle?: any;
+  showArrow?: boolean;
+}
+
+const SettingItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingItemsProps) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className="flex flex-row items-center justify-between "
+  >
+    <View className="flex gap-2 flex-row items-center justify-center">
+      <Image source={icon} className="size-6" />
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>
+        {title}
+      </Text>
+    </View>
+
+    {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+  </TouchableOpacity>
+);
 
 const Profile = () => {
+  const { user, refetch } = useGlobalContext();
+
+  const handleLogout = async () => {
+    const result = await logout();
+
+    if (result) {
+      Alert.alert("Success", "You have successfully logged out");
+    } else {
+      Alert.alert("Error", "Failed to log out");
+    }
+  };
+
   return (
     <SafeAreaView className="h-full bg-white">
       <ScrollView
@@ -17,16 +70,38 @@ const Profile = () => {
         </View>
         <View className="flex-row justify-center flex mt-5">
           <View className="flex-col flex items-center relative mt-5">
-            <Image source={images.avatar} className="size-44" />
-            <TouchableOpacity className="absolute bg-white rounded-xl p-2 bottom-10 -right-5">
-              <Image source={icons.edit} className="size-9" />
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-44 rounded-full"
+            />
+            <TouchableOpacity className="absolute p-2 bottom-8 -right-2">
+              <Image source={icons.edit} className="size-7" />
             </TouchableOpacity>
             <Text className="text-xl  font-rubik-semibold mt-2">
-              Abhishek kumar
+              {user?.name}
             </Text>
           </View>
         </View>
-        <View></View>
+        <View className="flex flex-col gap-4 mt-8">
+          <SettingItem icon={icons.calendar} title="My Bookings" />
+          <SettingItem icon={icons.wallet} title="Payments" />
+        </View>
+
+        <View className="flex flex-col gap-6 mt-4 border-t pt-5 border-primary-200">
+          {settings.slice(2).map((item, index) => (
+            <SettingItem key={index} icon={item.icon} title={item.title} />
+          ))}
+        </View>
+
+        <View className=" flex flex-col gap-6 mt-8 border-t pt-5 border-primary-200">
+          <SettingItem
+            icon={icons.logout}
+            title="Logout"
+            onPress={handleLogout}
+            textStyle="text-danger"
+            showArrow={false}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
