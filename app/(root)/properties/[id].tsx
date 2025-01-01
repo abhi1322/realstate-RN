f80bb3f26fd9d078b6a4c0c4623676b88c21ef69
Abhishek Kumar <abhi1322jii@gmail.com>
2025-01-01 13:26:17 +0530

    feat:property, agent section done

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -63,7 +63,10 @@ const Property = () => {
               source={{ uri: data?.image }}
               className="object-cover w-full h-[400px]"
             />
-            <TouchableOpacity className=" p-1 rounded-full flex flex-row justify-between absolute  top-20 left-4" onPress={()=> router.back()}>
+            <TouchableOpacity
+              className=" p-1 rounded-full flex flex-row justify-between absolute  top-20 left-4"
+              onPress={() => router.back()}
+            >
               <Image
                 source={icons.backArrow}
                 tintColor={"#000"}
@@ -83,51 +86,76 @@ const Property = () => {
               />
             </View>
           </View>
-          {/* Title, type, rating, reviews, beds, bath, sqft */}\
-          <View className="w-full px-4 mt-4">
-            <Text className="text-2xl font-rubik-bold">{data?.name}</Text>
+          <View className="px-4 mt-4">
+            {/* Title, type, rating, reviews, beds, bath, sqft */}\
+            <View className="w-fullmt-4">
+              <Text className="text-2xl font-rubik-bold">{data?.name}</Text>
 
-            <View className="flex flex-row items-center gap-2 mt-2">
-              <Text className="text-primary-300 font-rubik-medium uppercase text-xs rounded-full border border-primary-100  bg-primary-200 px-4  py-2">
-                {data?.type}
-              </Text>
-              <View className="flex flex-row items-center ">
-                <Image source={icons.star} className="object-cover ml-2" />
-                <Text className="ml-2 text-black-200">{data?.rating}</Text>
-                <Text className="ml-1 text-s font-rubik text-black-100">
-                  ({data?.reviews.length} reviews)
+              <View className="flex flex-row items-center gap-2 mt-2">
+                <Text className="text-primary-300 font-rubik-medium uppercase text-xs rounded-full border border-primary-100  bg-primary-200 px-4  py-2">
+                  {data?.type}
                 </Text>
+                <View className="flex flex-row items-center ">
+                  <Image source={icons.star} className="object-cover ml-2" />
+                  <Text className="ml-2 text-black-200">{data?.rating}</Text>
+                  <Text className="ml-1 text-s font-rubik text-black-100">
+                    ({data?.reviews.length} reviews)
+                  </Text>
+                </View>
+              </View>
+              <View className="mt-8 flex flex-row gap-8">
+                <View className="flex flex-row items-center gap-1">
+                  <Image
+                    source={icons.bed}
+                    className="object-cover ml-2 size-6 "
+                  />
+                  <Text className="ml-2 font-rubik-medium text-black-200">
+                    {data?.bedrooms} Beds
+                  </Text>
+                </View>
+                <View className="flex flex-row items-center gap-1">
+                  <Image
+                    source={icons.bath}
+                    className="object-cover ml-2 size-6 "
+                  />
+                  <Text className="ml-2 font-rubik-medium text-black-200">
+                    {data?.bedrooms} Bath
+                  </Text>
+                </View>
+                <View className="flex flex-row items-center gap-1">
+                  <Image
+                    source={icons.area}
+                    className="object-cover ml-2 size-6 "
+                  />
+                  <Text className="ml-2 font-rubik-medium text-black-200">
+                    {data?.area} sqft
+                  </Text>
+                </View>
               </View>
             </View>
-            <View className="mt-8 flex flex-row gap-8">
-              <View className="flex flex-row items-center gap-1">
+            {/* Agent section */}
+            <View className="mt-12">
+              <Text className="text-2xl font-rubik-bold">Agent</Text>
+              <View className="mt-4 flex flex-row justify-between items-center">
                 <Image
-                  source={icons.bed}
-                  className="object-cover ml-2 size-6 "
+                  source={{ uri: data?.agent.avatar }}
+                  className="size-20 rounded-full"
                 />
-                <Text className="ml-2 font-rubik-medium text-black-200">
-                  {data?.bedrooms} Beds
-                </Text>
-              </View>
-              <View className="flex flex-row items-center gap-1">
-                <Image
-                  source={icons.bath}
-                  className="object-cover ml-2 size-6 "
-                />
-                <Text className="ml-2 font-rubik-medium text-black-200">
-                  {data?.bedrooms} Beds
-                </Text>
-              </View>
-              <View className="flex flex-row items-center gap-1">
-                <Image
-                  source={icons.area}
-                  className="object-cover ml-2 size-6 "
-                />
-                <Text className="ml-2 font-rubik-medium text-black-200">
-                  {data?.bedrooms} Beds
-                </Text>
+                <View className="flex-1 ml-4">
+                  <Text className="text-xl font-rubik-medium text-black-200">
+                    {data?.agent.name}
+                  </Text>
+                  <Text className="text-s font-rubik text-black-100">
+                    Ownner
+                  </Text>
+                </View>
+                <View className="flex flex-row gap-4">
+                  <Image source={icons.chat} className="size-9 " />
+                  <Image source={icons.phone} className="size-9" />
+                </View>
               </View>
             </View>
+            {/* overview */}
           </View>
         </ScrollView>
       )}
